e1347489a019c045e45ac36cef50e4df90cd4440
callmekenta <kentahattori@hotmail.com>
2021-04-17 15:55:59 -0400

    socket
    -Added sockets to nodejs

--- hub.js
+++ hub.js
@@ -4,6 +4,17 @@ let app = express();
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
 
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
+
+io.on('connection', function(socket){
+  console.log('user connected');
+  
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
+});
+
 app.get('/', function(request, response){
   response.sendFile(__dirname + '/public/Login.html');
 });
