b6949c4d82379da8fe3f55f639f02f8b382a0bb2
ManmeetChoudhary <manmeet.choudhary@gmail.com>
2021-04-18 14:57:59 -0400

    login

--- hub.js
+++ hub.js
@@ -1,8 +1,23 @@
 let express = require('express');
 let app = express();
 
+const session = require('express-session');
+const { v4: uuidv4 } = require('uuid');
+const bcrypt = require('bcryptjs');
+
+const model = require('./public/model/model.js');
+
+
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
+
+app.use(session({
+  genid: uuidv4,
+  resave: false,
+  saveUninitialized: false,
+  // cookie: {secure: true},
+  secret: '3230 examples typing fast need coffee'
+}));
 
 let http = require('http').createServer(app);
 let io = require('socket.io')(http);
@@ -19,7 +34,37 @@ app.get('/', function(request, response){
   response.sendFile(__dirname + '/public/Login.html');
 });
 
+app.get('/login', function(request, response){
+  let user = request.body.loginUser;
+  console.log("got itttttttttttttttttttt");
+  console.log(user);
+});
+
+app.post('/create', function(request, response) {
+  let userData = {
+      username: request.body.username,
+      password: request.body.password,
+  };
+
+
+  model.User.find({username: request.body.username,}).then(function(userList) {
+      if (userList.length > 0) {
+      } else {
+          // there is no student with that sid
+          let newUser = new model.User(userData);
+          newUser.save(function(error) {
+              if (error) {
+                  console.error('Unable to add student:', error);
+              } else {
+                  console.log('User added');
+              }
+          });
+      }
+  });
+
+});
+
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function() {
     console.log(`Listening for requests on port ${app.get('port')}.`);
-});
+}); 
---
+++ public/model/model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/foodie', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, function(error) {
+    if (error) {
+        console.error('Unable to connect: ', error);
+    } else {
+        console.log('Connected to MongoDB');
+    }
+});
+mongoose.set('useCreateIndex', true);
+
+let Schema = mongoose.Schema;
+let userSchema = new Schema({
+    username: String,
+    hashPassword: String
+}, {
+    collection: 'users'
+});
+
+
+
+module.exports.User = mongoose.model('user', userSchema);
